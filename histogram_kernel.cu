4a1678a86e328aa9808040829c5233bbd728e2c0
Daniel Floo <daniel.flood@student.uts.edu.au>
2023-11-22 12:54:16 +1100

    removed hardcoding of nBins

--- histogram_kernel.cu
+++ histogram_kernel.cu
@@ -53,23 +53,27 @@ __global__ void buildSortedLinkmap(float *tensor, unsigned int *linkMap, float *
       // Compute bin index
       int binIndex = min((unsigned int)(value), nBins - 1);
       // Increment and retrieve the number of pixel in said bin
-      int localIndex = atomicAdd(&localIndexes[(channel * 256) + binIndex], 1);
+      //      int localIndex = atomicAdd(&localIndexes[(channel * 256) + binIndex], 1);
+      int localIndex = atomicAdd(&localIndexes[(channel * nBins) + binIndex], 1);
       // Retrieve the number of pixel in all bin lower (in cummulative histogram)
-      unsigned int lowerPixelCount = cumulativeHistogram[(channel * 256) + binIndex];
+      //      unsigned int lowerPixelCount = cumulativeHistogram[(channel * 256) + binIndex];
+      unsigned int lowerPixelCount = cumulativeHistogram[(channel * nBine) + binIndex];
       // Set the linkmap for indes to it's position as "pseudo-sorted"
       linkMap[index] = lowerPixelCount + localIndex;
     }
 }
 
-__global__ void rebuild(float *tensor, unsigned int *linkMap, float *targetHistogram, float scale, unsigned int channels, unsigned int tensorSize)
+__global__ void rebuild(float *tensor, unsigned int *linkMap, float *targetHistogram, float scale, unsigned int channels, unsigned int tensorSize, unsigned int nBins)
 {
   unsigned int index = threadIdx.x + blockIdx.x* blockDim.x;
   if (index < channels * tensorSize)
     {
       unsigned int channel = index / tensorSize;
       unsigned int value = 0;
-      for (int i=0 ; i < 256 ; ++i)
-	if (linkMap[index] >= targetHistogram[(channel * 256) + i] * scale) value = i;
+      //      for (int i=0 ; i < 256 ; ++i)
+      //	if (linkMap[index] >= targetHistogram[(channel * 256) + i] * scale) value = i;
+      for (int i=0 ; i < nBins ; ++i)
+	if (linkMap[index] >= targetHistogram[(channel * nBins) + i] * scale) value = i;
       tensor[index] = (float)value;
     }
 }
@@ -133,7 +137,7 @@ void matchHistogram(at::Tensor &featureMaps, at::Tensor &targetHistogram)
   at::Tensor max = torch::max_values(unsqueezed, 1, true).cuda();
 
   buildSortedLinkmap<<<(c*n) / THREAD_COUNT + 1, THREAD_COUNT>>>(featureMaps.data<float>(), linkMap, featuresHistogram.data<float>(), localIndexes, randomIndices[featureMaps.numel()].data<long>(), min.data<float>(), max.data<float>(), c, n, nBins);
-  rebuild<<<(c*n) / THREAD_COUNT + 1, THREAD_COUNT>>>(featureMaps.data<float>(), linkMap, targetHistogram.data<float>(), scale, c, n);
+  rebuild<<<(c*n) / THREAD_COUNT + 1, THREAD_COUNT>>>(featureMaps.data<float>(), linkMap, targetHistogram.data<float>(), scale, c, n, nBins);
 
   featureMaps.div_(float(nBins));
   
